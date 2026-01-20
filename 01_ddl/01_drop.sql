fed3eb4ece16d927e4041c31120ca8d82ec1fafd
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2026-01-20 16:44:33 -0300

    Add script to drop specified database tables
    This script drops specified tables from the database, including constraints.

--- 01_ddl/01_drop.sql
+++ 01_ddl/01_drop.sql
@@ -1,1 +1,13 @@
+-- 01_drop.sql
+-- Execute se quiser recriar do zero.
 
+BEGIN
+  FOR t IN (SELECT table_name FROM user_tables
+            WHERE table_name IN (
+              'TB_VENDA_ITEM','TB_VENDA','TB_PRODUTO','TB_CATEGORIA',
+              'TB_CLIENTE','TB_VENDEDOR','TB_AUDITORIA_VENDA','TB_CALENDARIO'
+            ))
+  LOOP
+    EXECUTE IMMEDIATE 'DROP TABLE ' || t.table_name || ' CASCADE CONSTRAINTS PURGE';
+  END LOOP;
+END;
