af790fdf56b42ad3dd466366b1b14ed50ef7cf7d
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2026-01-20 16:17:12 -0300

    Add SQL settings for date and timestamp formats

---
+++ 00_pre/00_settings.sql
@@ -0,0 +1,4 @@
+-- 00_settings.sql
+-- Ajustes úteis no SQL Developer (não obrigatório)
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+ALTER SESSION SET NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF';
